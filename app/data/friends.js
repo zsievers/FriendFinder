2a48eb9f8cef947115e4c09697996f1bc2979ad7
Zach <zjsievers@gmail.com>
2020-04-30 12:51:33 -0400

    added list of friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,45 @@
-{
-    "name", "Ahmed",
-    "photo", "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores", [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+var friendsArray = [
+    {
+        name: "Jim",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Joe",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Evan",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Blake",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Kyle",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Preston",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Tee",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    },
+    {
+        name: "Andy",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,4,1,2,4,2,1,4,2,1]
+    }
+];
+
+module.exports = friendsArray;
+  
