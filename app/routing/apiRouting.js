a80d68b322e5dd292d05c66e2c4afc4971efc19a
Zach <zjsievers@gmail.com>
2020-04-30 14:15:14 -0400

    set up html's && apiRouting

---
+++ app/routing/apiRouting.js
@@ -0,0 +1,15 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+    
+
+
+
+}
