f655d63c7c362fc410e80883a96a45ffee5cdf5e
Zach <zjsievers@gmail.com>
2020-05-03 13:01:22 -0400

    adjusted pictures and scores in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,53 @@
-// adjust scores & pictures
 var friendsData = [
     {
-        name: "Jim",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
+        name: "Hank",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/2048x3071/australian-cattle-dog.jpg?resize=980:*",
         scores: ["5","4","1","2","4","2","1","4","2","1"]
     },
     {
-        name: "Joe",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Rocky",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/2560x3839/rottweiler.jpg?resize=980:*",
+        scores: ["1","2","1","2","1","2","1","1","2","1"]
     },
     {
-        name: "Evan",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Roxie",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/papillion.jpg?crop=1xw:0.9997166336072542xh;center,top&resize=980:*",
+        scores: ["1","2","3","4","5","5","4","3","2","1"]
     },
     {
-        name: "Blake",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Cooper",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/3200x4799/labrador-retriever.jpg?resize=980:*",
+        scores: ["4","4","1","2","4","2","2","4","2","1"]
     },
     {
-        name: "Kyle",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Delilah",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/shetland-sheepdog.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        scores: ["3","4","1","2","4","2","2","3","1","1"]
     },
     {
-        name: "Preston",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Dodger",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/doberman-pinscher.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        scores: ["1","4","1","2","4","3","1","4","4","5"]
     },
     {
-        name: "Tee",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Lady",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/golden-retriever.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        scores: ["5","4","3","2","4","2","1","2","4","1"]
     },
     {
-        name: "Andy",
-        photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fg5138%2Fbest-family-dogs%2F&psig=AOvVaw0tzdsAE3lkXWpDfB86iX9S&ust=1588438108540000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCWu5uPk-kCFQAAAAAdAAAAABAD",
-        scores: ["5","4","1","2","4","2","1","4","2","1"]
+        name: "Kydon",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/german-shepher.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        scores: ["1","4","1","2","4","5","1","4","2","5"]
+    },
+    {
+        name: "Fluff",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/poodle.jpg?crop=1.0xw:1xh;center,top&resize=980:*",
+        scores: ["2","4","2","2","4","2","1","5","2","1"]
+    },
+    {
+        name: "Luna",
+        photo: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/40/1024x1535/border-collie.jpg?resize=980:*",
+        scores: ["2","1","2","4","4","1","3","5","5","1"]
     }
 ];
 
