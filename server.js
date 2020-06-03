fb7c77724749162130e4316bd55c8b08e41e8d6b
Zach <zjsievers@gmail.com>
2020-06-03 12:43:34 -0400

    fixed styling

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 var express  = require("express");
 
 var path = require('path');
+
 // creating express server
 var app = express();
 // heroku || local
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("app/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
